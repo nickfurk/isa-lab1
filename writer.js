becd130bec588629582180dbb753864bbbd3a860
April Cheng <acheng118@my.bcit.ca>
2023-09-14 21:35:38 -0700

    Add Message Class to writer.js

--- writer.js
+++ writer.js
@@ -1,81 +1,78 @@
-const addNoteButton = document.getElementById('button-add-note');
-const noteSection = document.getElementById('notes-section');
-const updateTimeSection = document.getElementById('update-time');
+const addNoteButton = document.getElementById("button-add-note");
+const noteSection = document.getElementById("notes-section");
+const updateTimeSection = document.getElementById("update-time");
 
-function createMessage(){
-    //create div
-    const messageDiv = document.createElement('div');
-    messageDiv.className = 'note-div';
+// Class Message
+function Message() {
+  this.div = document.createElement("div");
+  this.div.className = "note-div";
 
-    //create textarea
-    const textarea = document.createElement('textarea');
-    textarea.className = "textarea-message";
-    textarea.placeholder = "Enter note here";
+  this.textarea = document.createElement("textarea");
+  this.textarea.className = "textarea-message";
+  this.textarea.placeholder = "Enter note here";
 
-    //create remove button
-    const removeBtn = document.createElement('button');
-    removeBtn.innerText = 'Remove';
-    removeBtn.className = 'remove-button';
+  this.removeBtn = document.createElement("button");
+  this.removeBtn.innerText = "Remove";
+  this.removeBtn.className = "remove-button";
 
-    //add click event listner to the remove button
-    removeBtn.addEventListener('click', () => {
+  this.div.appendChild(this.textarea);
+  this.div.appendChild(this.removeBtn);
 
-        //remove the component from the HTML
-        noteSection.removeChild(messageDiv);
-    });
+  // --- event listener ---
+  this.removeBtn.addEventListener("click", () => {
+    this.remove();
+  });
 
-    messageDiv.appendChild(textarea);
-    messageDiv.appendChild(removeBtn);
+  // --- method ---
+  this.remove = () => {
+    // Remove the Message from the DOM
+    noteSection.removeChild(this.div);
+  };
+}
 
-    return messageDiv
+function updateTime() {
+  const currentTime = new Date();
+  updateTimeSection.textContent = `stored at: ${currentTime.toLocaleTimeString()}`;
 }
 
 function updateLocalStorage() {
-    const notesArray = []
-    const allTextareas = document.querySelectorAll(".textarea-message");
-    let counter = 1
+  const notesArray = [];
+  const allTextareas = document.querySelectorAll(".textarea-message");
+  let counter = 1;
 
-    //for each textarea, add it to the array as an obj
-    allTextareas.forEach((textArea) => {
-   
-        notesArray.push(
-            {id: counter++,
-            text: textArea.value})
-    })
+  //for each textarea info, add it to the array as an obj
+  allTextareas.forEach((textArea) => {
+    notesArray.push({ id: counter++, text: textArea.value });
+  });
 
-    //convert notesArray JSON obj to string and store in local storage
-    localStorage.setItem('notes', JSON.stringify(notesArray));
+  //convert notesArray JSON obj to string and store in local storage
+  localStorage.setItem("notes", JSON.stringify(notesArray));
 
-    //update the time
-    const currentTime = new Date();
-    updateTimeSection.textContent = `stored at: ${currentTime.toLocaleTimeString()}`;
+  updateTime();
 }
 
 //Update the local storage every 2 seconds
 setInterval(updateLocalStorage, 2000);
 
-//Add new message logic
-addNoteButton.addEventListener('click', () => {
-    //create new message obj
-    const newMessage = createMessage();
-
-    //append new message to the note section
-    noteSection.appendChild(newMessage);
-})
+//Add new message event listner
+addNoteButton.addEventListener("click", () => {
+  const newMessage = new Message();
+  noteSection.appendChild(newMessage.div);
+});
 
 // Retrieve stored data from local storage on page load
-window.addEventListener('load', () => {
-    if (localStorage.getItem('notes')) {
-        messages = JSON.parse(localStorage.getItem('notes'));
+window.addEventListener("load", () => {
+  if (localStorage.getItem("notes")) {
+    messages = JSON.parse(localStorage.getItem("notes"));
 
-        // Clear the current noteSection
-        noteSection.innerHTML = '';
+    // Clear the current noteSection
+    noteSection.innerHTML = "";
 
-        // Repopulate the HTML note-section
-        messages.forEach((message) => {
-            const messageElement = createMessage();
-            messageElement.querySelector('textarea').value = message.text;
-            noteSection.appendChild(messageElement);
-        });
-    }
+    // Repopulate the HTML note-section
+    messages.forEach((message) => {
+      const messageElement = new Message();
+      messageElement.textarea.value = message.text;
+      noteSection.appendChild(messageElement.div);
+    });
+  }
 });
