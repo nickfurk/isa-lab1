df464454897b7e5449814766c1d6439b16544392
April Cheng <acheng118@my.bcit.ca>
2023-09-14 21:37:15 -0700

    Add Message Class to reader.js

--- reader.js
+++ reader.js
@@ -1,35 +1,34 @@
-const noteSection = document.getElementById('notes-section');
-const updateTimeSection = document.getElementById('update-time');
+const noteSection = document.getElementById("notes-section");
+const updateTimeSection = document.getElementById("update-time");
 
-function updateTime(){
-    const currentTime = new Date();
-    updateTimeSection.textContent = `updated at: ${currentTime.toLocaleTimeString()}`;
+// Class Message
+function Message(messageText) {
+  this.div = document.createElement("div");
+  this.div.className = "div-readers-note";
+  this.div.textContent = messageText;
 }
 
-function createMessage(messageText) {
-    const messageDiv = document.createElement('div');
-    messageDiv.className = 'div-readers-note';
-
-    messageDiv.textContent = messageText;
-    return messageDiv;
+function updateTime() {
+  const currentTime = new Date();
+  updateTimeSection.textContent = `updated at: ${currentTime.toLocaleTimeString()}`;
 }
 
 function showMessage() {
-    if (localStorage.getItem('notes')) {
-        const messages = JSON.parse(localStorage.getItem('notes'));
+  if (localStorage.getItem("notes")) {
+    const messages = JSON.parse(localStorage.getItem("notes"));
 
-        // Clear the current noteSection
-        noteSection.innerHTML = '';
+    // Clear the current noteSection
+    noteSection.innerHTML = "";
 
-        // Repopulate the HTML note-section
-        messages.forEach((message) => {
-            if (message.text) {
-                const messageElement = createMessage(message.text);
-                noteSection.appendChild(messageElement);
-            }
-        });
-        updateTime();
-    }
+    // Repopulate the HTML note-section
+    messages.forEach((message) => {
+      if (message.text) {
+        const messageElement = new Message(message.text);
+        noteSection.appendChild(messageElement.div);
+      }
+    });
+    updateTime();
+  }
 }
 
 showMessage();
